3574dda9b74836892eb26bdc483bded76f69b9f4
Chrispian <chrispian.burks@webdevstudios.com>
2019-03-01 10:03:20 -0600

    Initial commit.

---
+++ wp-discogs.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ *  Plugin to pull in Discogs collection, wishlist, etc.
+ *
+ * @link              http://www.chrispian.com
+ * @since             1.0.0
+ * @package           Ausm_Plugin
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP Discogs
+ * Plugin URI:        http://www.chrispian.com
+ * Description:       Uses Discogs API to pull in data from discogs.com and insert into WordPress
+ * Version:           1.0.0
+ * Author:            Chrispian H. Burks
+ * Author URI:        http://www.chrispian.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-discogs
+ * Domain Path:       /languages
+ *
+ * Todo:
+ *
+ * - Create settings for discogs API authorization
+ *
+ */
+
+namespace CHB_WP_Discogs;
+
